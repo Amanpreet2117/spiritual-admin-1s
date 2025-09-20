cef53763a30a76cfd8d6e48a36b2024a93cc21ac
rubaab <https://Amanpreet2117@github.com>
2025-09-20 23:58:37 +0530

    url changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,7 +7,7 @@ class ApiClient {
   private baseURL: string;
 
   constructor() {
-    this.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+    this.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://spiritual-article-back-end.onrender.com';
     
     this.client = axios.create({
       baseURL: this.baseURL,
